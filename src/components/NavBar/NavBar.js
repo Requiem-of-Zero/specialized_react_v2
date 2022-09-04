c04cc082c3937d8af0d76ed256ca48c0389e7756
Sam Wong <theesamwong@gmail.com>
2022-09-03 23:15:21 -0700

    navbar created and styled

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,38 +2,89 @@ import React from "react";
 import {
   NavBarWrapper,
   NavBarContainer,
+  NavBarSectionContainer,
   SecondaryNavBarContainer,
   SecondaryNavRightCol,
   SignInLink,
   LanguageLink,
-  RetailerLink
+  RetailerLink,
+  LeftSection,
+  MiddleSection,
+  RightSection,
+  NavBarLink,
+  ShoppingCartLink,
+  WishlistLink,
+  SearchLink,
+  Logo,
 } from "./NavBar.styles";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import GlobalStyle from "../../pages/global.css";
 
 const NavBar = ({ categories }) => {
   return (
     <NavBarWrapper>
       <GlobalStyle />
-      <SecondaryNavBarContainer>
-        <p>
-          <a href="#Shipping">Free Shipping on orders over $50</a>
-        </p>
-        <SecondaryNavRightCol>
-          <ul>
-            <li>
-              <RetailerLink href="#Retailer">Find a Retailer</RetailerLink>
-            </li>
-            <li>
-              <img src="https://www.specialized.com/_ui/addons/specializedstorefrontaddon/desktop/common/images/flags/country-US.svg" alt="language" />
-              <LanguageLink href="#En">En</LanguageLink>
-            </li>
-            <li>
-              <SignInLink href="#SignIn">Sign In</SignInLink>
-            </li>
-          </ul>
-        </SecondaryNavRightCol>
-      </SecondaryNavBarContainer>
-      <NavBarContainer></NavBarContainer>
+      <NavBarContainer>
+        <SecondaryNavBarContainer>
+          <p>
+            <a href="#Shipping">Free Shipping on orders over $50*</a>
+          </p>
+          <SecondaryNavRightCol>
+            <ul>
+              <li>
+                <RetailerLink href="#Retailer">
+                  <LocationOnIcon className="location-icon" />
+                  Find a Retailer
+                </RetailerLink>
+              </li>
+              <li>
+                <img
+                  src="https://www.specialized.com/_ui/addons/specializedstorefrontaddon/desktop/common/images/flags/country-US.svg"
+                  alt="language"
+                />
+                <LanguageLink href="#En">En</LanguageLink>
+              </li>
+              <li>
+                <SignInLink href="#SignIn">Sign In</SignInLink>
+              </li>
+            </ul>
+          </SecondaryNavRightCol>
+        </SecondaryNavBarContainer>
+        <NavBarSectionContainer>
+          <LeftSection>
+            <a href="">
+              <Logo src="https://cdn.shopify.com/s/files/1/0904/4132/files/spec-logo-bp.jpg?v=1585000290" />
+            </a>
+          </LeftSection>
+          <MiddleSection>
+            <ul>
+              {categories.map((category, i) => {
+                return (
+                  <li>
+                    <NavBarLink href={category.categoryUrl}>
+                      {category.categoryTitle}
+                    </NavBarLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </MiddleSection>
+          <RightSection>
+            <SearchLink href="#shopping_cart">
+              <SearchIcon />
+            </SearchLink>
+            <WishlistLink href="#shopping_cart">
+              <FavoriteBorderIcon />
+            </WishlistLink>
+            <ShoppingCartLink href="#shopping_cart">
+              <ShoppingCartIcon className="shopping_cart" />
+            </ShoppingCartLink>
+          </RightSection>
+        </NavBarSectionContainer>
+      </NavBarContainer>
     </NavBarWrapper>
   );
 };
--- src/components/NavBar/NavBar.styles.js
+++ src/components/NavBar/NavBar.styles.js
@@ -5,9 +5,13 @@ export const NavBarWrapper = styled.div`
   top: 0;
   width: 100%;
   height: 32px;
-  `;
-  
-  export const SecondaryNavBarContainer = styled.div`
+`;
+
+export const NavBarContainer = styled.div`
+  width: inherit;
+`;
+
+export const SecondaryNavBarContainer = styled.div`
   overflow: hidden;
   background: black;
   height: inherit;
@@ -57,10 +61,80 @@ export const SignInLink = styled.a`
 `;
 
 export const LanguageLink = styled.a`
-padding: 0 45px 0 5px;
+  padding: 0 45px 0 5px;
 `;
 
 export const RetailerLink = styled.a`
+  display: flex;
+  align-items: center;
+  .location-icon {
+    font-size: 16px;
+    padding-right: 10px;
+  }
 `;
 
-export const NavBarContainer = styled.div``;
+export const NavBarSectionContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  height: 64px;
+  justify-content: space-between;
+  padding: 0 45px;
+`;
+
+export const LeftSection = styled.div``;
+
+export const Logo = styled.img`
+  width: 150px;
+`;
+
+export const MiddleSection = styled.div`
+  ul {
+    display: flex;
+    justify-content: space-between;
+    list-style: none;
+    text-align: center;
+    font-size: 12px;
+    li{
+        padding: 18px 24px;
+    }
+  }
+  width: 600px;
+`;
+
+export const RightSection = styled.div`
+  a {
+    &:hover {
+      color: red;
+      transition: all 0.3s ease;
+    }
+  }
+`;
+
+export const NavBarLink = styled.a`
+  color: black;
+  text-decoration: none;
+  text-transform: uppercase;
+  :hover {
+    color: red;
+    transition: all 0.3s ease;
+  }
+`;
+
+export const WishlistLink = styled.a`
+  color: black;
+  padding: 13px 16px;
+`;
+
+export const SearchLink = styled.a`
+  color: black;
+  padding: 13px 16px;
+`;
+
+export const ShoppingCartLink = styled.a`
+  color: black;
+  padding: 13px 9px;
+  .shopping_cart {
+    transform: scaleX(-1);
+  }
+`;
+
